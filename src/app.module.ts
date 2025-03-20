de458d4305b7c483d346315ade1eb4977ccfd5d2
Esteban Velazco <dev.velazco.esteban11@gmail.com>
2025-03-20 12:14:04 -0700

    inicializacion de proyecto desde cero

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsuariosService } from './usuarios/usuarios.service';
-import { UsuariosController } from './usuarios/usuarios.controller';
-import { UsuariosModule } from './usuarios/usuarios.module';
-import { AuthController } from './auth/auth.controller';
-import { AuthModule } from './auth/auth.module';
-import { TestController } from './test/test.controller';
 
 
-const typeOrm = TypeOrmModule.forRoot({
-  type: 'postgres',
-  host: '192.168.10.52', 
-  port: 5432,
-  username: 'postgres',
-  password: 'password',
-  database: 'antilavado',
-  synchronize: true,
-  autoLoadEntities: true,
-  logging: true
-});
 
 @Module({
-  imports: [typeOrm, UsuariosModule, AuthModule],
-  controllers: [AppController, UsuariosController, AuthController, TestController],
-  providers: [AppService, UsuariosService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/usuarios/interfaces/permiso.interface.ts
+++
@@ -1,6 +0,0 @@
-
-
-export interface Permiso { 
-    id: number;
-    permiso: string;
-  }
