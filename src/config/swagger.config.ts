4ea1a6f29835ba29e8efaf9be75997a0d419134c
Esteban Velazco <dev.velazco.esteban11@gmail.com>
2025-03-25 12:25:50 -0700

    correccion

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,14 +1,8 @@
 import { DocumentBuilder } from "@nestjs/swagger";
 
 export const swaggerConfig = new DocumentBuilder()
-<<<<<<< HEAD
     .setTitle('Api Antilavado')
     .setDescription('Endpoints descriptivos de la api del proyecto Ley Antilavado pd: tqm silman <3')
     .setVersion('1.0')
     // .addTag('cats')
-=======
-    .setTitle('Api Template')
-    .setDescription('Template de la api, con autorizacion y auntenticacion')
-    .setVersion('1.0')
->>>>>>> 467a9f21369b07cc47a90d8bdce58fcaf2bd259b
     .build();
